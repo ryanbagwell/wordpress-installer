a671e83a7866e87bc35c1bb87f19e54a9a71c590
Ryan Bagwell <ryan@ryanbagwell.com>
2014-09-05 13:17:04 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+setup(name='wordpress-installer',
+      version='0.0.1',
+      description='A command-line tool for installing a WordPress project',
+      long_description="A command-line tool for installing a WordPress project",
+      author='Ryan Bagwell',
+      author_email='ryan@ryanbagwell.com',
+      url='https://github.com/ryanbagwell',
+      py_modules=['wpinstaller'],
+      scripts=['wordpresspm/wpinstaller'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: PHP',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+      ]
+    )
